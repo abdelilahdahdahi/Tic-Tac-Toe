d2fe23345edab51efbc6436ff14050c367da9739
abdelilah <dahdahiabdelilah2002@gmail.com>
2024-04-02 01:26:29 +0200

    managing state of player names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setplayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing(editing => !editing);
+  }
+
+  function handleChange(event) {
+    setplayerName(event.target.value);
+  }
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
-              <span className="player-symbol">{symbol}</span>
+            {!isEditing && <span className="player-name">{playerName}</span>}
+            {isEditing && <input type="text" required value={playerName} onChange={handleChange}/>}              
+            <span className="player-symbol">{symbol}</span>
             </span>
-            <button>edit</button>
+            <button onClick={handleEditClick}>
+              {isEditing ? 'Save' : 'Edit'}
+            </button>
           </li>
     )
 }
